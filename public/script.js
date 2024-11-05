52b0934b5e8134836aa7ea66ffd9cb357ae222a1
Ntlahla Pikwa <ntlahlapikwa80@gmail.com>
2024-11-05 12:35:34 -0800

    updated my server and Script.js

--- public/script.js
+++ public/script.js
@@ -2,14 +2,15 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("List-container");
 
 function addTask() {
-    
+   
     if (inputBox.value === '') {
         alert("You must write something!");
+        saveData();
     } else {
-       
+        
         let li = document.createElement("li");
 
-      
+       
         li.innerHTML = `
             ${inputBox.value}
             <button class="remove-btn" onclick="removeTask(this)">Remove</button>
@@ -21,15 +22,38 @@ function addTask() {
       
         li.addEventListener("click", () => {
             li.classList.toggle("checked");
+            saveData();
         });
 
-      
+       
         inputBox.value = '';
+
+      
+        saveData();
+    }
+}
+
+function removeTask(button) {
+   
+    const li = button.parentElement;
+    li.remove();
+    
+   
+    saveData();
+}
+
+function saveData() {
+   
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        listContainer.innerHTML = savedData;
     }
 }
 
 
-function removeTask(button) {
-    const li = button.parentElement;
-    li.remove();
-}
+window.onload = showTask;
