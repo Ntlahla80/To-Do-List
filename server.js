74a2c6a1733a52a9936ae64ab3bacb1518a0f60a
Ntlahla Pikwa <ntlahlapikwa80@gmail.com>
2024-11-05 12:36:44 -0800

    added saveData and showTask

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// server.js
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Serve static files (CSS, JS, images) from the 'public' directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the HTML file when the root URL is accessed
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
